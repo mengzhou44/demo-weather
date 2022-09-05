7fe5ff9744831e0ffce04740bb96955da5aa6a2b
Meng Zhou <v-menzhou@expediagroup.com>
2022-09-05 08:52:37 -0500

    add frame motion

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import './_app.css'
+import './_app.css';
+import { motion } from 'framer-motion';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps, router }) {
+  return (
+    <motion.div
+      key={router.route}
+      initial="pageInitial"
+      animate="pageAnimate"
+      variants={{
+        pageInitial: {
+          opacity: 0,
+        },
+        pageAnimate: {
+          opacity: 1,
+        },
+      }}
+    >
+      <Component {...pageProps} />
+    </motion.div>
+  );
 }
 
-export default MyApp
+export default MyApp;
