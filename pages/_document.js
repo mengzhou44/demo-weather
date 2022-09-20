d7753cb41f30cf56d4fe40630ab204c0e650eaf2
Meng Zhou <v-menzhou@expediagroup.com>
2022-09-19 20:54:10 -0500

    commit

--- pages/_document.js
+++ pages/_document.js
@@ -12,9 +12,9 @@ class MyDocument extends Document {
             crossOrigin="true"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&display=swap"
             rel="stylesheet"
-          />
+          ></link>
         </Head>
         <body>
           <Main />
