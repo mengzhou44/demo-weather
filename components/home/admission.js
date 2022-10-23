f9c7e29c9c306dbc37f00b4e277b85b693d12121
meng <mengzhou44@gmail.com>
2022-10-22 22:01:34 -0600

    commit

--- components/home/admission.js
+++ components/home/admission.js
@@ -4,7 +4,6 @@ import styles from './admission.module.css'
 const Admission = () => {
   return (
     <section id="admission" className= {styles.container}>
- 
       <div className={styles.content}>      
         <div className="title">
           <h2>
