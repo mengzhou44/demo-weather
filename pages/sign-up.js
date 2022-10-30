0da074bbc467ab04f076bf41169e95c7aaa0f4a7
meng <mengzhou44@gmail.com>
2022-10-29 20:36:33 -0600

    commit

--- pages/sign-up.js
+++ pages/sign-up.js
@@ -118,6 +118,13 @@ const SignUp = () => {
         </Link>
       </header>
       <main className={styles.main}>
+      <div className={styles.close}>
+        <Link href="/">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+          </Link>
+        </div>
         <h2 className={styles.title}>Sign Up</h2>
         <div className={styles.inputs}>
           <div className={styles.field}>
